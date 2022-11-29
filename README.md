dd6fbef9f4ae4ec8c7aa12edc65e3f489c43ef5d
kunal910 <76044724+kunal910@users.noreply.github.com>
2022-11-29 22:48:54 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Weather-App
+# React Weather App - ReactJS Projects
+
+![image](https://user-images.githubusercontent.com/60633026/204522226-3193bb0f-0c3b-48d1-9b42-290d27048fcf.png)
+![image](https://user-images.githubusercontent.com/60633026/204522429-ac6b68dd-4bab-45ff-b6d2-b9558ca595a2.png)
+
+### How to run
+* Clone the git repository by using git clone command.
+* Run npm i command.
+* Run npm start command.
+
+### APIs Used
+[Open Weather APIs](https://openweathermap.org/)
+
+https://openweathermap.org/current
+
+### API Info
+* Method: `GET`
+* URL: `https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}`
+
+### Libraries used
+* `styled-components`
+* `axios`
---
+++ src/modules/WeatherInfoComponent.js
@@ -0,0 +1,103 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export const WeatherInfoIcons = {
+    humidity: "/react-weather-app/icons/humidity.svg",
+    wind: "/react-weather-app/icons/wind.svg",
+};
+const Location = styled.span`
+  margin: 15px auto;
+  text-transform: capitalize;
+  font-size: 28px;
+  font-weight: bold;
+`;
+const Condition = styled.span`
+  margin: 20px auto;
+  text-transform: capitalize;
+  font-size: 14px;
+  & span {
+    font-size: 28px;
+  }
+`;
+const WeatherInfoLabel = styled.span`
+  margin: 20px 25px 10px;
+  text-transform: capitalize;
+  text-align: start;
+  width: 90%;
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+const WeatherContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 30px auto;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const WeatherInfoContainer = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  margin: 5px 10px;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const InfoIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+const InfoLabel = styled.span`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  margin: 15px;
+  & span {
+    font-size: 12px;
+    text-transform: capitalize;
+  }
+`;
+
+const WeatherInfoComponent = (props) => {
+    const {name, value} = props;
+    return (
+        <InfoContainer>
+            <InfoIcon src={WeatherInfoIcons[name]}/>
+            <InfoLabel>
+                {value}
+                <span>{name}</span>
+            </InfoLabel>
+        </InfoContainer>
+    );
+};
+const WeatherComponent = (props) => {
+    const {weather} = props;
+    return (
+        <>
+            <WeatherContainer>
+                <Condition>
+                    <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
+                    {`  |  ${weather?.weather[0].description}`}
+                </Condition>
+            </WeatherContainer>
+            <Location>{`${weather?.name}, ${weather?.sys?.country}`}</Location>
+            <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
+            <WeatherInfoContainer>
+                <WeatherInfoComponent name={"humidity"} value={weather?.main?.humidity}/>
+                <WeatherInfoComponent name={"wind"} value={weather?.wind?.speed}/>
+            </WeatherInfoContainer>
+        </>
+    );
+};
+
+export default WeatherComponent;
